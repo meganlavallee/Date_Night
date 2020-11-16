b7d2fad23e6e760fcda458b7cf35602e5d209312
Xis <rick-s@hotmail.com>
2020-11-16 14:11:44 -0800

    merge fix

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+var quertyURL: "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+
+$.ajax({
+    url: 'queryURL',
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+        console.log(data);
+    },
+    error: function (error) {
+        console.log(`Error ${error}`);
+    }
+});
 getDronk();
-function getDronk() {
-  // const apikey = "1";
-  const queryDrink = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-
-  $.ajax({
-    method: "GET",
-    url: queryDrink,
-  }).then(function (dronk) {
-    console.log(dronk);
-    console.log(dronk.drinks[0].strIngredient1);
-  });
-}
-
