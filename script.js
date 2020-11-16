9a86ba7ab7b4018f6a2b0dd4048ecaed9c72617e
Xis <rick-s@hotmail.com>
2020-11-16 15:49:12 -0800

    merge conflict

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var quertyURL: "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
 
 $.ajax({
     url: 'queryURL',
